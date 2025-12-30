444c6610b9ca7710d7eb67d03c5be268dd198ab3
solaar45 <92338974+solaar45@users.noreply.github.com>
2025-12-30 20:33:16 +0100

    Add Account schema

---
+++ backend/lib/wealth_backend/accounts/account.ex
@@ -0,0 +1,28 @@
+defmodule WealthBackend.Accounts.Account do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "accounts" do
+    field :name, :string
+    field :type, Ecto.Enum,
+      values: [:checking, :savings, :credit_card, :brokerage, :insurance, :cash, :other]
+    field :currency, :string
+    field :is_active, :boolean, default: true
+    field :opened_at, :date
+    field :closed_at, :date
+
+    belongs_to :user, WealthBackend.Accounts.User
+    belongs_to :institution, WealthBackend.Accounts.Institution
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(account, attrs) do
+    account
+    |> cast(attrs, [:name, :type, :currency, :is_active, :opened_at, :closed_at, :user_id, :institution_id])
+    |> validate_required([:name, :type, :currency, :user_id])
+    |> foreign_key_constraint(:user_id)
+    |> foreign_key_constraint(:institution_id)
+  end
+end
