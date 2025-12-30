d59b007205020278eda25652bec7108cc7e7b255
solaar45 <92338974+solaar45@users.noreply.github.com>
2025-12-30 21:03:56 +0100

    Add ChangesetJSON for validation errors

---
+++ backend/lib/wealth_backend_web/controllers/changeset_json.ex
@@ -0,0 +1,18 @@
+defmodule WealthBackendWeb.ChangesetJSON do
+  @doc """
+  Renders changeset errors.
+  """
+  def error(%{changeset: changeset}) do
+    # When encoded, the changeset returns its errors
+    # as a JSON object. So we just pass it forward.
+    %{errors: translate_errors(changeset)}
+  end
+
+  defp translate_errors(changeset) do
+    Ecto.Changeset.traverse_errors(changeset, fn {msg, opts} ->
+      Regex.replace(~r"%{(\w+)}", msg, fn _, key ->
+        opts |> Keyword.get(String.to_existing_atom(key), key) |> to_string()
+      end)
+    end)
+  end
+end
