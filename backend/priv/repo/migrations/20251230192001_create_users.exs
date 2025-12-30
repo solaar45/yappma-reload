352c8e1146d2e086db52d3f8464f7b9007fff3a5
solaar45 <92338974+solaar45@users.noreply.github.com>
2025-12-30 20:31:27 +0100

    Add users migration

---
+++ backend/priv/repo/migrations/20251230192001_create_users.exs
@@ -0,0 +1,15 @@
+defmodule WealthBackend.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :name, :string, null: false
+      add :email, :string, null: false
+      add :currency_default, :string, default: "EUR", null: false
+
+      timestamps(type: :utc_datetime)
+    end
+
+    create unique_index(:users, [:email])
+  end
+end
