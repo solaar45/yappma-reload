2913871fecb10a9903bcd91c48b9cdd52509d215
solaar45 <92338974+solaar45@users.noreply.github.com>
2025-12-30 22:23:20 +0100

    Simplify tailwind.config.ts - remove require.resolve

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,28 +1,19 @@
-import { join } from 'path';
 import type { Config } from 'tailwindcss';
 import { skeleton } from '@skeletonlabs/tw-plugin';
 
 const config = {
-	// 1. Set the dark mode to class
 	darkMode: 'class',
 	content: [
 		'./src/**/*.{html,js,svelte,ts}',
-		// 2. Append the path for the Skeleton NPM package and files:
-		join(require.resolve(
-			'@skeletonlabs/skeleton'),
-			'../**/*.{html,js,svelte,ts}'
-		)
+		'./node_modules/@skeletonlabs/skeleton/**/*.{html,js,svelte,ts}'
 	],
 	theme: {
 		extend: {},
 	},
 	plugins: [
-		// 3. Append the Skeleton plugin (no forms/typography to avoid errors if missing)
 		skeleton({
 			themes: {
-				preset: [
-					{ name: "cerberus", enhancements: true }
-				]
+				preset: [{ name: 'cerberus', enhancements: true }]
 			}
 		})
 	]
