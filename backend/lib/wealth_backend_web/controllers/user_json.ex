43c750f51cecfe0f1606a9c71c8d1e9fe2c1da09
solaar45 <92338974+solaar45@users.noreply.github.com>
2025-12-30 21:00:39 +0100

    Add UserJSON view

---
+++ backend/lib/wealth_backend_web/controllers/user_json.ex
@@ -0,0 +1,28 @@
+defmodule WealthBackendWeb.UserJSON do
+  alias WealthBackend.Accounts.User
+
+  @doc """
+  Renders a list of users.
+  """
+  def index(%{users: users}) do
+    %{data: for(user <- users, do: data(user))}
+  end
+
+  @doc """
+  Renders a single user.
+  """
+  def show(%{user: user}) do
+    %{data: data(user)}
+  end
+
+  defp data(%User{} = user) do
+    %{
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      currency_default: user.currency_default,
+      inserted_at: user.inserted_at,
+      updated_at: user.updated_at
+    }
+  end
+end
