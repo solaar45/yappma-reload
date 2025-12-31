d19598c50df2b622d01906cb2c4febfc094b5420
solaar45 <92338974+solaar45@users.noreply.github.com>
2025-12-31 20:05:35 +0100

    Remove padding from DashboardPage for full-width layout

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -19,7 +19,7 @@ export default function DashboardPage() {
   }
 
   return (
-    <div className="flex flex-1 flex-col gap-4 p-4 md:p-6">
+    <div className="flex flex-1 flex-col gap-4 p-4">
       {/* Metric Cards Grid */}
       <div className="grid auto-rows-min gap-4 md:grid-cols-3">
         {/* Net Worth Card */}
