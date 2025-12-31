bf2d2962832e213baa8901b684e4647f5b6a95da
solaar45 <92338974+solaar45@users.noreply.github.com>
2025-12-31 12:14:42 +0100

    Export all CRUD hooks

--- frontend/src/lib/api/hooks/index.ts
+++ frontend/src/lib/api/hooks/index.ts
@@ -2,3 +2,7 @@ export { useDashboard } from './useDashboard';
 export { useAccounts } from './useAccounts';
 export { useAssets } from './useAssets';
 export { useCreateSnapshot } from './useCreateSnapshot';
+export { useCreateAccount } from './useCreateAccount';
+export { useCreateAsset } from './useCreateAsset';
+export { useUpdateAccount, useUpdateAsset } from './useUpdateAccount';
+export { useDeleteAccount, useDeleteAsset } from './useDeleteAccount';
