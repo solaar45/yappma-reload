2e4f3b11c14441baa99465355c45732ce36970dc
solaar45 <92338974+solaar45@users.noreply.github.com>
2026-01-02 20:48:06 +0100

    fix: remove duplicate Encrypted.Binary module
    - Module already defined in lib/wealth_backend/encrypted.ex
    - Removes compilation error from duplicate definition

--- backend/lib/wealth_backend/encrypted/binary.ex
+++
@@ -1,8 +0,0 @@
-defmodule WealthBackend.Encrypted.Binary do
-  @moduledoc """
-  Encrypted binary field type using Cloak.
-  Automatically encrypts data on write and decrypts on read.
-  """
-
-  use Cloak.Ecto.Binary, vault: WealthBackend.Vault
-end
