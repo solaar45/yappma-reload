84d3b84a9091cd0d06b1869110586e32c1a4a525
solaar45 <92338974+solaar45@users.noreply.github.com>
2026-01-02 01:21:04 +0100

    fix: add Tesla HTTP client dependency

--- backend/mix.exs
+++ backend/mix.exs
@@ -59,7 +59,9 @@ defmodule WealthBackend.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
       {:bandit, "~> 1.5"},
-      {:cors_plug, "~> 3.0"}
+      {:cors_plug, "~> 3.0"},
+      # FinTS Integration
+      {:tesla, "~> 1.8"}
     ]
   end
 
