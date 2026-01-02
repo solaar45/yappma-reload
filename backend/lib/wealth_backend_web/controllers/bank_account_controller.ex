b16335d314e161d55516cb68b0e17a31983068a8
solaar45 <92338974+solaar45@users.noreply.github.com>
2026-01-02 21:03:54 +0100

    feat: add bank account mapping endpoint
    - POST /api/bank_accounts/:id/map endpoint
    - Map BankAccount to Account
    - Unmap with null account_id
    - Returns updated bank account with preloaded account

--- backend/lib/wealth_backend_web/controllers/bank_account_controller.ex
+++ backend/lib/wealth_backend_web/controllers/bank_account_controller.ex
@@ -1,21 +1,59 @@
 defmodule WealthBackendWeb.BankAccountController do
   use WealthBackendWeb, :controller
 
-  alias WealthBackend.BankConnections
+  alias WealthBackend.FinTS
 
   action_fallback WealthBackendWeb.FallbackController
 
-  def create(conn, %{"bank_account" => bank_account_params}) do
-    with {:ok, bank_account} <- BankConnections.create_bank_account(bank_account_params) do
-      conn
-      |> put_status(:created)
-      |> json(%{success: true, data: bank_account})
+  @doc """
+  List bank accounts for a connection.
+  GET /api/bank_accounts?bank_connection_id=:id
+  """
+  def index(conn, %{"bank_connection_id" => bank_connection_id}) do
+    bank_accounts = FinTS.list_bank_accounts(bank_connection_id)
+    render(conn, :index, bank_accounts: bank_accounts)
+  end
+
+  def index(conn, _params) do
+    conn
+    |> put_status(:bad_request)
+    |> json(%{error: "bank_connection_id parameter is required"})
+  end
+
+  @doc """
+  Get a single bank account.
+  GET /api/bank_accounts/:id
+  """
+  def show(conn, %{"id" => id}) do
+    bank_account = FinTS.get_bank_account!(id)
+    render(conn, :show, bank_account: bank_account)
+  end
+
+  @doc """
+  Map a bank account to an internal account.
+  POST /api/bank_accounts/:id/map
+  
+  Body:
+  {
+    "account_id": 123  // or null to unmap
+  }
+  """
+  def map_account(conn, %{"id" => id, "account_id" => account_id}) do
+    case FinTS.link_bank_account(id, account_id) do
+      {:ok, bank_account} ->
+        render(conn, :show, bank_account: bank_account)
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        conn
+        |> put_status(:unprocessable_entity)
+        |> put_view(json: WealthBackendWeb.ChangesetJSON)
+        |> render(:error, changeset: changeset)
     end
   end
 
-  def link_account(conn, %{"id" => id, "account_id" => account_id}) do
-    with {:ok, bank_account} <- BankConnections.link_bank_account(id, account_id) do
-      json(conn, %{success: true, data: bank_account})
-    end
+  def map_account(conn, _params) do
+    conn
+    |> put_status(:bad_request)
+    |> json(%{error: "account_id is required"})
   end
 end
