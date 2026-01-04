bca653731830bfdd142e4063e6be20bcacfacaaf
solaar45 <92338974+solaar45@users.noreply.github.com>
2026-01-04 21:29:19 +0100

    fix: Update config files for Yappma and add Repo configuration
    - Update config/dev.exs with Yappma.Repo config
    - Update config/test.exs
    - Update config/runtime.exs for production
    - Configure database properly

--- backend/config/runtime.exs
+++ backend/config/runtime.exs
@@ -7,22 +7,6 @@ import Config
 # any compile-time configuration in here, as it won't be applied.
 # The block below contains prod specific runtime configuration.
 
-# ## Using releases
-#
-# If you use `mix release`, you need to explicitly enable the server
-# by passing the PHX_SERVER=true when you start it:
-#
-#     PHX_SERVER=true bin/wealth_backend start
-#
-# Alternatively, you can use `mix phx.gen.release` to generate a `bin/server`
-# script that automatically sets the env var above.
-if System.get_env("PHX_SERVER") do
-  config :wealth_backend, WealthBackendWeb.Endpoint, server: true
-end
-
-config :wealth_backend, WealthBackendWeb.Endpoint,
-  http: [port: String.to_integer(System.get_env("PORT", "4000"))]
-
 if config_env() == :prod do
   database_url =
     System.get_env("DATABASE_URL") ||
@@ -33,12 +17,10 @@ if config_env() == :prod do
 
   maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
 
-  config :wealth_backend, WealthBackend.Repo,
+  config :yappma, Yappma.Repo,
     # ssl: true,
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    # For machines with several cores, consider starting multiple pools of `pool_size`
-    # pool_count: 4,
     socket_options: maybe_ipv6
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
@@ -54,49 +36,21 @@ if config_env() == :prod do
       """
 
   host = System.get_env("PHX_HOST") || "example.com"
+  port = String.to_integer(System.get_env("PORT") || "4000")
 
-  config :wealth_backend, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
+  config :yappma, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
-  config :wealth_backend, WealthBackendWeb.Endpoint,
+  config :yappma, YappmaWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0}
+      ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      port: port
     ],
     secret_key_base: secret_key_base
 
-  # ## SSL Support
-  #
-  # To get SSL working, you will need to add the `https` key
-  # to your endpoint configuration:
-  #
-  #     config :wealth_backend, WealthBackendWeb.Endpoint,
-  #       https: [
-  #         ...,
-  #         port: 443,
-  #         cipher_suite: :strong,
-  #         keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-  #         certfile: System.get_env("SOME_APP_SSL_CERT_PATH")
-  #       ]
-  #
-  # The `cipher_suite` is set to `:strong` to support only the
-  # latest and more secure SSL ciphers. This means old browsers
-  # and clients may not be supported. You can set it to
-  # `:compatible` for wider support.
-  #
-  # `:keyfile` and `:certfile` expect an absolute path to the key
-  # and cert in disk or a relative path inside priv, for example
-  # "priv/ssl/server.key". For all supported SSL configuration
-  # options, see https://hexdocs.pm/plug/Plug.SSL.html#configure/1
-  #
-  # We also recommend setting `force_ssl` in your config/prod.exs,
-  # ensuring no data is ever sent via http, always redirecting to https:
-  #
-  #     config :wealth_backend, WealthBackendWeb.Endpoint,
-  #       force_ssl: [hsts: true]
-  #
-  # Check `Plug.SSL` for all available options in `force_ssl`.
+  # Styx URL for production
+  config :yappma,
+    styx_base_url: System.get_env("STYX_URL") || "http://styx:8093"
 end
--- backend/config/test.exs
+++ backend/config/test.exs
@@ -1,23 +1,19 @@
 import Config
 
 # Configure your database
-#
-# The MIX_TEST_PARTITION environment variable can be used
-# to provide built-in test partitioning in CI environment.
-# Run `mix help test` for more information.
-config :wealth_backend, WealthBackend.Repo,
+config :yappma, Yappma.Repo,
   username: "postgres",
   password: "postgres",
   hostname: "localhost",
-  database: "wealth_backend_test#{System.get_env("MIX_TEST_PARTITION")}",
+  database: "yappma_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
-config :wealth_backend, WealthBackendWeb.Endpoint,
+config :yappma, YappmaWeb.Endpoint,
   http: [ip: {127, 0, 0, 1}, port: 4002],
-  secret_key_base: "5ZMgYICZf1HytBlV/GZRhsHu5RAtGx4bhu/UjKuxinC6vplzNwLbDa3M1fwxAmyn",
+  secret_key_base: "test-secret-key-base-at-least-64-bytes-long-for-security-purposes",
   server: false
 
 # Print only warnings and errors during test
@@ -25,7 +21,3 @@ config :logger, level: :warning
 
 # Initialize plugs at runtime for faster test compilation
 config :phoenix, :plug_init_mode, :runtime
-
-# Sort query params output of verified routes for robust url comparisons
-config :phoenix,
-  sort_verified_routes_query_params: true
