49d5a567bb1da62ef8037f9c36380b5fe6569597
solaar45 <92338974+solaar45@users.noreply.github.com>
2026-01-05 00:21:59 +0100

    fix: Add all missing routes (Assets, Institutions, Snapshots) and integrate Bank Connections in navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { DashboardPage } from './pages/DashboardPage';
 import { AccountsPage } from './pages/AccountsPage';
-import { BankCallbackPage } from './pages/BankCallbackPage';
+import { AssetsPage } from './pages/AssetsPage';
+import { InstitutionsPage } from './pages/InstitutionsPage';
+import { SnapshotsPage } from './pages/SnapshotsPage';
 import { BankConnectionsPage } from './pages/BankConnectionsPage';
+import { BankCallback } from './pages/BankCallback';
 import { Layout } from './components/Layout';
 
 function App() {
@@ -10,11 +13,14 @@ function App() {
     <BrowserRouter>
       <Routes>
         {/* Bank callback doesn't need layout */}
-        <Route path="/bank-callback" element={<BankCallbackPage />} />
+        <Route path="/bank-callback" element={<BankCallback />} />
         
         {/* Main app routes with layout */}
         <Route path="/" element={<Layout><DashboardPage /></Layout>} />
         <Route path="/accounts" element={<Layout><AccountsPage /></Layout>} />
+        <Route path="/assets" element={<Layout><AssetsPage /></Layout>} />
+        <Route path="/institutions" element={<Layout><InstitutionsPage /></Layout>} />
+        <Route path="/snapshots" element={<Layout><SnapshotsPage /></Layout>} />
         <Route path="/bank-connections" element={<Layout><BankConnectionsPage /></Layout>} />
       </Routes>
     </BrowserRouter>
