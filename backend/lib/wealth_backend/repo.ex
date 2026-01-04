45f80be9642c6502e07aa61ddb9d6a83645f4e4a
solaar45 <92338974+solaar45@users.noreply.github.com>
2026-01-04 23:40:40 +0100

    fix: Make WealthBackend.Repo an alias for Yappma.Repo

--- backend/lib/wealth_backend/repo.ex
+++ backend/lib/wealth_backend/repo.ex
@@ -1,5 +1,30 @@
+# WealthBackend.Repo is an alias for the configured Yappma.Repo
+# This allows both naming conventions to work in the codebase
 defmodule WealthBackend.Repo do
-  use Ecto.Repo,
-    otp_app: :wealth_backend,
-    adapter: Ecto.Adapters.Postgres
+  # Simply forward all calls to the actual configured repo
+  defdelegate all(queryable, opts \\ []), to: Yappma.Repo
+  defdelegate get(queryable, id, opts \\ []), to: Yappma.Repo
+  defdelegate get!(queryable, id, opts \\ []), to: Yappma.Repo
+  defdelegate get_by(queryable, clauses, opts \\ []), to: Yappma.Repo
+  defdelegate get_by!(queryable, clauses, opts \\ []), to: Yappma.Repo
+  defdelegate one(queryable, opts \\ []), to: Yappma.Repo
+  defdelegate one!(queryable, opts \\ []), to: Yappma.Repo
+  defdelegate insert(struct_or_changeset, opts \\ []), to: Yappma.Repo
+  defdelegate insert!(struct_or_changeset, opts \\ []), to: Yappma.Repo
+  defdelegate update(changeset, opts \\ []), to: Yappma.Repo
+  defdelegate update!(changeset, opts \\ []), to: Yappma.Repo
+  defdelegate delete(struct_or_changeset, opts \\ []), to: Yappma.Repo
+  defdelegate delete!(struct_or_changeset, opts \\ []), to: Yappma.Repo
+  defdelegate insert_or_update(changeset, opts \\ []), to: Yappma.Repo
+  defdelegate insert_or_update!(changeset, opts \\ []), to: Yappma.Repo
+  defdelegate insert_all(schema_or_source, entries, opts \\ []), to: Yappma.Repo
+  defdelegate update_all(queryable, updates, opts \\ []), to: Yappma.Repo
+  defdelegate delete_all(queryable, opts \\ []), to: Yappma.Repo
+  defdelegate preload(struct_or_structs, preloads, opts \\ []), to: Yappma.Repo
+  defdelegate transaction(fun_or_multi, opts \\ []), to: Yappma.Repo
+  defdelegate rollback(value), to: Yappma.Repo
+  defdelegate aggregate(queryable, aggregate, opts \\ []), to: Yappma.Repo
+  defdelegate exists?(queryable, opts \\ []), to: Yappma.Repo
+  defdelegate reload(struct, opts \\ []), to: Yappma.Repo
+  defdelegate reload!(struct, opts \\ []), to: Yappma.Repo
 end
