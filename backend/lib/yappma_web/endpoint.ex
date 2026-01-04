64a4fa0ca0bb34384225c665de66111b39e2569e
solaar45 <92338974+solaar45@users.noreply.github.com>
2026-01-04 21:29:35 +0100

    fix: Add YappmaWeb.Endpoint configuration
    - Create YappmaWeb.Endpoint module
    - Configure proper Phoenix endpoint

---
+++ backend/lib/yappma_web/endpoint.ex
@@ -0,0 +1,51 @@
+defmodule YappmaWeb.Endpoint do
+  use Phoenix.Endpoint, otp_app: :yappma
+
+  # The session will be stored in the cookie and signed,
+  # this means its contents can be read but not tampered with.
+  # Set :encryption_salt if you would also like to encrypt it.
+  @session_options [
+    store: :cookie,
+    key: "_yappma_key",
+    signing_salt: "yappma_signing_salt",
+    same_site: "Lax"
+  ]
+
+  socket "/live", Phoenix.LiveView.Socket,
+    websocket: [connect_info: [session: @session_options]]
+
+  # Serve at "/" the static files from "priv/static" directory.
+  #
+  # You should set gzip to true if you are running phx.digest
+  # when deploying your static files in production.
+  plug Plug.Static,
+    at: "/",
+    from: :yappma,
+    gzip: false,
+    only: YappmaWeb.static_paths()
+
+  # Code reloading can be explicitly enabled under the
+  # :code_reloader configuration of your endpoint.
+  if code_reloading? do
+    socket "/phoenix/live_reload/socket", Phoenix.LiveReload.Socket
+    plug Phoenix.LiveReload
+    plug Phoenix.CodeReloader
+  end
+
+  plug Phoenix.LiveDashboard.RequestLogger,
+    param_key: "request_logger",
+    cookie_key: "request_logger"
+
+  plug Plug.RequestId
+  plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
+
+  plug Plug.Parsers,
+    parsers: [:urlencoded, :multipart, :json],
+    pass: ["*/*"],
+    json_decoder: Phoenix.json_library()
+
+  plug Plug.MethodOverride
+  plug Plug.Head
+  plug Plug.Session, @session_options
+  plug YappmaWeb.Router
+end
